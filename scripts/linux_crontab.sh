22374aac4a1a6e44acef7ec6dad1c507bf632298
sahargavriely <gav6sa@gmail.com>
2024-06-29 11:32:09 +0300

    scripts fixing

--- scripts/linux_crontab.sh
+++ scripts/linux_crontab.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
-    (crontab -l 2>/dev/null; echo "@reboot ${PWD}/scripts/linux_run.sh") | crontab
+    (crontab -l 2>/dev/null; echo "@reboot ${PWD}/scripts/linux_reboot.sh") | crontab
 }
 
 
--- scripts/linux_reboot.sh
+++ scripts/linux_reboot.sh
@@ -7,7 +7,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 function main {
     git pull --prune
     ./scripts/linux_install.sh
-    ./scripts/linux_run.sh
+    while [ ! -f stop ]; do ./scripts/linux_run.sh; done
 }
 
 
